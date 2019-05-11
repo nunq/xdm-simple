828c22db4a5a507314dbd75ff368466451e487f6
hyphenc <46054695+hyphenc@users.noreply.github.com>
2019-05-11 19:59:51 +0200

    improved install.sh, more consistent prompt in Xresources

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "checking dependencies...\n"
+sudo pacman -S --needed bash terminus-font ttf-bitstream-vera xorg-xclock xorg-xdm xorg-xkill xorg-xmessage xorg-xsetroot xorg-xwininfo
+sudo mkdir /etc/X11/xdm/simple || exit 1
 sudo cp buttons xdm-config Xresources Xsetup Xstartup /etc/X11/xdm/simple/
 sudo chmod 0755 /etc/X11/xdm/simple/{Xsetup,Xstartup,buttons}
 sudo install -Dm0644 xdm-simple.service /usr/lib/systemd/system/xdm-simple.service
