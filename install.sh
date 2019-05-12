3fb4b98a711c79d94a19a6a4807410ea99b94e31
hyphenc <46054695+hyphenc@users.noreply.github.com>
2019-05-12 21:09:21 +0200

    fix install.sh, remove font dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "checking dependencies...\n"
-sudo pacman -S --needed bash terminus-font ttf-bitstream-vera xorg-xclock xorg-xdm xorg-xkill xorg-xmessage xorg-xsetroot xorg-xwininfo
-sudo mkdir /etc/X11/xdm/simple || exit 1
+echo "checking dependencies..."
+sudo pacman -S --needed bash xorg-xclock xorg-xdm xorg-xkill xorg-xmessage xorg-xsetroot xorg-xwininfo
+sudo mkdir /etc/X11/xdm/simple
 sudo cp buttons xdm-config Xresources Xsetup Xstartup /etc/X11/xdm/simple/
 sudo chmod 0755 /etc/X11/xdm/simple/{Xsetup,Xstartup,buttons}
 sudo install -Dm0644 xdm-simple.service /usr/lib/systemd/system/xdm-simple.service
