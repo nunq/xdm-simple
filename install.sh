55253a86da1c6a243326695213bac78f010a601e
hyphenc <46054695+hyphenc@users.noreply.github.com>
2019-05-12 10:13:25 +0200

    disable tty2

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo cp buttons xdm-config Xresources Xsetup Xstartup /etc/X11/xdm/simple/
 sudo chmod 0755 /etc/X11/xdm/simple/{Xsetup,Xstartup,buttons}
 sudo install -Dm0644 xdm-simple.service /usr/lib/systemd/system/xdm-simple.service
 sudo systemctl enable xdm-simple.service
+sudo systemctl disable getty@tty2.service # where xdm spawns
 echo "done"
